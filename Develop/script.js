154c0465aa141b1ee14bb85e9c45b2fff4c002c7
Floki <ypr_tylerkeuler@aol.com>
2022-05-25 13:42:01 -0400

    adding save button listner and local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,14 @@
 // Todays day and Date
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+//save Button click listner 
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        // Get values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save  in local storage
+        localStorage.setItem(time, text);
+    })
