1adbb383a8f35fa6af86955eb25b1dc2c8de5be7
Floki <ypr_tylerkeuler@aol.com>
2022-05-26 13:50:46 -0400

    sudosudosudosudo

--- Develop/script.js
+++ Develop/script.js
@@ -2,3 +2,7 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
+// save to local storage 
+
+// change color for current time, past time, and furture timne. 
+
