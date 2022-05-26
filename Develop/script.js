8f97e90b4172fc5c404789067e99cf6598a94027
Floki <ypr_tylerkeuler@aol.com>
2022-05-26 14:25:00 -0400

    setting up an object to accept user imput to save to local storage for later use

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,47 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-// save to local storage 
+// declaring an object to store data based on current task/time. 
+let planner = [
+    {
+        taskNumber: 9,
+        taskValue: "", //task value is blank to allow for user input. 
+    },
+    {
+        taskNumber: 10,
+        taskValue: "", //task value is blank to allow for user input. 
+    },
+    {
+        taskNumber: 11,
+        taskValue: "", //task value is blank to allow for user input. 
+    },
+    {
+        taskNumber: 12,
+        taskValue: "", //task value is blank to allow for user input. 
+    },
+    {
+        taskNumber: 1,
+        taskValue: "", //task value is blank to allow for user input. 
+    },
+    {
+        taskNumber: 2,
+        taskValue: "", //task value is blank to allow for user input. 
+    }, 
+     {
+        taskNumber: 3,
+        taskValue: "", //task value is blank to allow for user input. 
+    },
+    {
+        taskNumber: 4,
+        taskValue: "", //task value is blank to allow for user input. 
+    },
+    {
+        taskNumber: 5,
+        taskValue: "", //task value is blank to allow for user input. 
+    },
+
+]
+console.log(todayDate)
 
 // change color for current time, past time, and furture timne. 
 
