ad17a611bb67860bede1a0d7fe3a4de2342e1b5c
Floki <ypr_tylerkeuler@aol.com>
2022-05-26 13:25:02 -0400

    changing the style on the time boxes

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,3 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-//save Button click listner 
-$(document).ready(function () {
-    $(".saveBtn").on("click", function () {
-        // Get values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        // Save  in local storage
-        localStorage.setItem(time, text);
-    })
