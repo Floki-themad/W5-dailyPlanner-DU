225435babe1d115e39b60e86669a324e3dd7b733
Floki <ypr_tylerkeuler@aol.com>
2022-06-03 17:56:48 -0400

    working through local storage issues

--- Develop/script.js
+++ Develop/script.js
@@ -2,47 +2,63 @@
 var todayDate = moment().format('dddd, MMM Do YYYY hh:mm');
 $("#currentDay").html(todayDate);
 
-$('button').click(function () {
-    console.log("you did it!!")
-    saveData();
-});
+var saveButton = document.querySelector("#saveButton");
+var hourNine = document.querySelector("#hourNine");
+var taskNine = document.querySelector("#taskNine");
 
-$(document).ready(function () {
-    $('.saveBtn').on('click', function () {
-        var value = $(this).siblings('.taskValue').val();
-        var time = $(this).parent().attr('id');
-        localStorage.setItem(time, value);
-        console.log(localStorage);
-    })
-});
+saveButton.addEventListener("click", function(event){
+    event.preventDefault();
+ var taskEvent = {
+     time: hourNine.value,
+     task: taskNine.value.trim()
+ };
+ localStorage.setItem("taskEvent", JSON.stringify(taskEvent));
+})
 
-function saveData(){
-    var time = document.querySelector("#hour9").value;
-    var input = document.querySelector("#task9").value;
+function loadData() {
+    var task = JSON.parse(localStorage.getItem("taskEvent"));
+    
+    //document.getElementById("#taskNine").textContent = task.taskNine;
+ 
+    
+    console.log(task);
 
-    localStorage.setItem("hour9", time);
-    localStorage.setItem("task9", input);
-}
-var timeOfDay = document.querySelector("#hour9");
-var taskOfDay = document.querySelector("#task9");
-
-function loadData(){
-    var currentTime = localStorage.getItem("#hour9").value;
-    var timeInput = localStorage.getItem("task9").value;
-
-    timeOfDay.textContent = currentTime;
-    taskOfDay.textContent = timeInput;
+ 
 }
 
-loadData();
+function init(){
+    loadData();
+
+}
+
+init();
+// $(document).ready(function () {
+//     console.log($(document).ready)
+//     $('.saveBtn').click(function (e) {
+//         e.preventDefault()
+//         const value = $("button").closest("#task9").val();
+//         const time = $(this).parent().attr('id');
+//         localStorage.setItem(time, value);
+//     })
+// });
+
+// var timeOfDay = document.querySelector("#hour9");
+// var taskOfDay = document.querySelector("#task9");
+
+// function loadData(){
+//     var currentTime = localStorage.getItem("hour9");
+//     var timeInput = localStorage.getItem("task9");
+//     console.log(currentTime);
+//     taskOfDay.value = currentTime;
+    
+// }
+
 
 // change color for current time, past time, and furture timne. 
 function colorTime() {
     var blockTime = 9;
     var currentHour = moment().hours();
     $('.block').each(function () {
-        console.log(currentHour);//console logs for bug fixing. 
-        console.log(blockTime);
         if (blockTime < currentHour) {
             $(this).addClass('past');
         } else if (blockTime === currentHour) {
@@ -54,10 +70,12 @@ function colorTime() {
             $(this).addClass('future');
         }
         blockTime += 1
+        console.log(blockTime);
+        console.log(currentHour);
     });
 }
 
 colorTime();
 
 // to check time to see if block colors should be updated. 
-var interval = setInterval(colorTime, 15000);
+//var interval = setInterval(colorTime, 15000);
